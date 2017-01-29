e9575bb5b6b1941ae521a308ecf65a2ba6c4382b
Matt Ruffner <mattruffner7@gmail.com>
2017-01-29 15:58:15 -0500

    more work on p2

--- p2/digosc.m
+++ p2/digosc.m
@@ -1,28 +1,44 @@
 fs=8000;
-fo=10;
-
-ampscale=pi/(fs/fo)*2;
-
-
-amp=2;
-
 tax=(0:fs-1)/fs;
 
-alpha=(2*pi*fo)/fs;
-
-% numerator
-b=[ampscale*amp 0 0];
-
-% denom
-a=[1 -2*cos(alpha) 1];
-
-
-% delta pulse to input energy
+% delta pulse to input energy, for testing
 del=[1 zeros(1,fs-1)];
 
+amps=[1 2];
+fos=[10 20];
 
-f1=filter(b,a,del); % excite oscillator
+ampscales=pi./(fs./fos)*2;
+alphas=(2*pi.*fos)/fs;
+
+
+
+% numerators
+bs=[ampscales(1)*amps(1) 0 0];
+if length(amps) > 1 
+    counter=2;
+    for n=amps(2:end)
+        bs=[bs; ampscales(counter)*n 0 0];
+        counter=counter+1;
+    end
+end
+
+as=[1 -2*cos(alphas(1)) 1];
+if length(alphas) > 1
+    for n=alphas(2:end)
+        as=[as; 1 -2*cos(n) 1];
+    end
+end
+    
+
+
+f1=filter(bs(1,1:end), as(1,1:end), del); % excite oscillator
+f2=filter(bs(2,1:end), as(2,1:end), del);
+
 
 [r1,t1]=impz(f1);
+[r2,t2]=impz(f2);
+
+hold on
 plot(t1,r1,'r--')
+plot(t2,r2,'b--')
 
